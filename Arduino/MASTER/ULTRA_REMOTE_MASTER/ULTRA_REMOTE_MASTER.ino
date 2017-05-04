9a9e22b320c1dc0a85d5d8eb285b72495cc1d502
I <hoboinabox7@gmail.com>
2017-05-04 16:42:53 -0400

    Apple TV functionality = 100 %

--- Arduino/MASTER/ULTRA_REMOTE_MASTER/ULTRA_REMOTE_MASTER.ino
+++ Arduino/MASTER/ULTRA_REMOTE_MASTER/ULTRA_REMOTE_MASTER.ino
@@ -19,13 +19,21 @@
 #include <IRremote.h>
 #include <SoftwareSerial.h>
 
-
+// PINS
 const int LEDPin = 8;
+int LED = 13;
+int RX = 10;
+int TX = 11;
+
+IRsend irsend;
+SoftwareSerial mySerial(RX, TX); // (10, 11) The RX and TX on aruidno and HC - 08 are switched
+
+
+
+// Data Variables
 
 char userInput;
-
 String device;
-
 
 
 String recvDataString = "";
@@ -33,587 +41,58 @@ String recvTotalDataString = "";
 
 int commandArrayLen = 0;
 
-String iterationsString = "";
-int iterationsNum = 0;
-int iterationsCount = 0;
+String numChunksString = "";
+int numChunks = 0;
+int numChunksCount = 0;
 
-////////////////////////
-//    Device Codes
-////////////////////////
-
-/*
-  // Apple TV
-  const unsigned int apple_play_pause[136] = {
-  13232, 9420,
-  4540, 640,
-  520, 640,
-  1640, 640,
-  1640, 640,
-  1660, 640,
-  500, 620,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1660, 620,
-  1680, 620,
-  1660, 620,
-  540, 600,
-  540, 620,
-  540, 620,
-  540, 600,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  1680, 620,
-  1680, 600,
-  540, 620,
-  1660, 620,
-  540, 620,
-  520, 620,
-  1680, 600,
-  1680, 620,
-  540, 620,
-  1660, 620,
-  540, 620,
-  1660, 620,
-  1680, 620,
-  34860, 9340,
-  4560, 620,
-  540, 600,
-  1680, 600,
-  1680, 620,
-  1680, 600,
-  540, 620,
-  1660, 620,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  1680, 620,
-  520, 620,
-  540, 620,
-  540, 620,
-  520, 620,
-  1680, 600,
-  1680, 620,
-  540, 600,
-  1680, 620,
-  520, 620,
-  540, 620,
-  540, 620,
-  520, 620,
-  540, 620,
-  540, 600,
-  1680,620,
-  1660, 620,
-  540, 620,
-  1660, 620,
-  540, 620,
-  1660, 620,
-  1680, 620
-                                           };
+boolean debugMode = false;
 
 
-
-  const unsigned int apple_menu[136] = {
-  11372, 9420,
-  4520, 640,
-  520, 640,
-  1640, 620,
-  1660, 620,
-  1680, 600,
-  540, 620,
-  1660, 620,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  1680, 620,
-  520, 620,
-  540, 620,
-  540, 620,
-  520, 620,
-  1680, 600,
-  1680, 620,
-  1660, 620,
-  540, 620,
-  520, 640,
-  520, 640,
-  500, 660,
-  500, 640,
-  520, 600,
-  540, 620,
-  1680, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  500, 660,
-  1640, 640,
-  1660, 640,
-  11372, 9420,
-  4520, 640,
-  520, 640,
-  1640, 620,
-  1660, 620,
-  1680, 600,
-  540, 620,
-  1660, 620,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  1680, 620,
-  520, 620,
-  540, 620,
-  540, 620,
-  520, 620,
-  1680, 600,
-  1680, 620,
-  1660, 620,
-  540, 620,
-  520, 640,
-  520, 640,
-  500, 660,
-  500, 640,
-  520, 600,
-  540, 620,
-  1680, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  500, 660,
-  1640, 640,
-  1660, 640};
-
-
-  const unsigned int apple_select[136] = {24532, 9420,
-  4540, 640,
-  500, 640,
-  1660, 620,
-  1660, 640,
-  1640, 640,
-  500, 660,
-  1640, 640,
-  1640, 640,
-  1640, 640,
-  1640, 640,
-  1660, 640,
-  1640, 640,
-  520, 600,
-  540, 620,
-  540, 600,
-  540, 620,
-  1680, 600,
-  540, 620,
-  540, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  540, 620,
-  1680, 600,
-  540, 620,
-  520, 620,
-  1680, 620,
-  1660, 620,
-  540, 600,
-  1680, 620,
-  540, 600,
-  1680, 620,
-  1680, 620,
-  37160, 9360,
-  4520, 660,
-  500, 640,
-  1640, 640,
-  1660, 620,
-  1660, 640,
-  520, 620,
-  1660, 640,
-  1640, 640,
-  1640, 640,
-  1660, 620,
-  1660, 640,
-  1640, 640,
-  500, 660,
-  500, 640,
-  520, 640,
-  500, 660,
-  1640, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  500, 640,
-  520, 640,
-  500, 660,
-  500, 640,
-  520, 640,
-  500, 640,
-  1660, 640,
-  1640, 640,
-  500, 640,
-  1640, 660,
-  500, 640,
-  1640, 660,
-  1660, 620
-                                       };
-
-  const unsigned int apple_up[136] = {
-  12828, 9420,
-  4540, 640,
-  500, 640,
-  1660, 640,
-  1640, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  1640, 640,
-  1660, 640,
-  1640, 640,
-  1640, 640,
-  1640, 660,
-  500, 640,
-  520, 620,
-  520, 640,
-  520, 640,
-  1640, 640,
-  500, 660,
-  1640, 640,
-  500, 660,
-  1640, 640,
-  500, 640,
-  520, 640,
-  520, 640,
-  500, 640,
-  520, 640,
-  1640, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  1660, 640,
-  12828, 9420,
-  4540, 640,
-  500, 640,
-  1660, 640,
-  1640, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  1640, 640,
-  1660, 640,
-  1640, 640,
-  1640, 640,
-  1640, 660,
-  500, 640,
-  520, 620,
-  520, 640,
-  520, 640,
-  1640, 640,
-  500, 660,
-  1640, 640,
-  500, 660,
-  1640, 640,
-  500, 640,
-  520, 640,
-  520, 640,
-  500, 640,
-  520, 640,
-  1640, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  520, 640,
-  1640, 640,
-  1660, 640};
-
-  const unsigned int apple_down[136] = {
-  12252, 9420,
-  4520, 640,
-  520, 640,
-  1640, 640,
-  1640, 640,
-  1660, 620,
-  520, 620,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  1680, 600,
-  560, 600,
-  540, 620,
-  520, 620,
-  540, 620,
-  1660, 620,
-  540, 620,
-  540, 600,
-  1680, 620,
-  1660, 620,
-  540, 620,
-  520, 620,
-  540, 620,
-  540, 600,
-  540, 620,
-  1680, 600,
-  1680, 600,
-  540, 620,
-  1680, 600,
-  540, 620,
-  1660, 620,
-  1700, 600,
-  12252, 9420,
-  4520, 640,
-  520, 640,
-  1640, 640,
-  1640, 640,
-  1660, 620,
-  520, 620,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  1680, 600,
-  560, 600,
-  540, 620,
-  520, 620,
-  540, 620,
-  1660, 620,
-  540, 620,
-  540, 600,
-  1680, 620,
-  1660, 620,
-  540, 620,
-  520, 620,
-  540, 620,
-  540, 600,
-  540, 620,
-  1680, 600,
-  1680, 600,
-  540, 620,
-  1680, 600,
-  540, 620,
-  1660, 620,
-  1700, 600};
-
-  const unsigned int apple_left[136] = {
-  14104, 9420,
-  4520, 660,
-  500, 640,
-  1640, 640,
-  1660, 600,
-  1680, 620,
-  540, 600,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  1680, 600,
-  1680, 620,
-  1660, 620,
-  540, 620,
-  520, 620,
-  540, 620,
-  540, 620,
-  1660, 620,
-  1660, 620,
-  540, 600,
-  540, 620,
-  1680, 600,
-  540, 620,
-  540, 620,
-  520, 620,
-  540, 620,
-  540, 620,
-  1660, 620,
-  1660, 620,
-  540, 600,
-  1680, 620,
-  540, 600,
-  1680, 620,
-  1680, 620,
-  50100, 9340,
-  4560, 620,
-  540, 600,
-  1680, 600,
-  1680, 620,
-  1660, 620,
-  540, 600,
-  1680, 620,
-  1660, 620,
-  1680, 600,
-  1680, 620,
-  1660, 620,
-  1660, 620,
-  540, 620,
-  540, 600,
-  540, 620,
-  540, 600,
-  1680, 620,
-  1660, 620,
-  540, 620,
-  520, 620,
-  1680, 600,
-  540, 620,
-  540, 620,
-  540, 600,
-  540, 620,
-  520, 620,
-  1680, 620,
-  1660, 620,
-  540, 600,
-  1680, 620,
-  540, 600,
-  1680, 600,
-  1700, 620};
-
-  const unsigned int apple_right[136] = {
-  30696, 9300,
-  4620, 560,
-  580, 560,
-  1700, 600,
-  1680, 600,
-  1720, 540,
-  600, 540,
-  1720, 580,
-  1740, 540,
-  1740, 580,
-  1680, 580,
-  1740, 540,
-  1740, 560,
-  600, 560,
-  580, 540,
-  640, 520,
-  600, 580,
-  1720, 540,
-  600, 560,
-  1740, 540,
-  1740, 540,
-  600, 580,
-  560, 560,
-  620, 540,
-  600, 540,
-  620, 540,
-  600, 580,
-  1700, 580,
-  1720, 560,
-  580, 580,
-  1720, 560,
-  580, 540,
-  1740, 560,
-  1760, 540,
-  30696, 9300,
-  4620, 560,
-  580, 560,
-  1700, 600,
-  1680, 600,
-  1720, 540,
-  600, 540,
-  1720, 580,
-  1740, 540,
-  1740, 580,
-  1680, 580,
-  1740, 540,
-  1740, 560,
-  600, 560,
-  580, 540,
-  640, 520,
-  600, 580,
-  1720, 540,
-  600, 560,
-  1740, 540,
-  1740, 540,
-  600, 580,
-  560, 560,
-  620, 540,
-  600, 540,
-  620, 540,
-  600, 580,
-  1700, 580,
-  1720, 560,
-  580, 580,
-  1720, 560,
-  580, 540,
-  1740, 560,
-  1760, 540};
-
-*/
-
-
-int LED = 13;
-
-int RX = 10;
-int TX = 11;
-
-
-IRsend irsend;
-SoftwareSerial mySerial(RX, TX); // (10, 11) The RX and TX on aruidno and HC - 08 are switched
 
 void setup() {
-
-
   pinMode(LEDPin, OUTPUT);
-
 
   Serial.begin(9600);
   mySerial.begin(9600);
 
   Serial.print("Ultra Remote Initiated\n");
+  
+  Serial.print("Enter d for debug mode\n");
+  Serial.print("Enter e to exit debug mode\n");
 }
 
 void loop() {
 
+  if (Serial.available() > 0) {
+    userInput = Serial.read();
 
-  /*
-    if (Serial.available() > 0) {
-
-      userInput = Serial.read();
-
-      switch (userInput) {
-
-        case 'A':
-          sendCommand(apple_play_pause, sizeof(apple_play_pause) / sizeof(apple_play_pause[0]));
-          break;
-
-        case 'B':
-          sendCommand(apple_menu, sizeof(apple_menu) / sizeof(apple_menu[0]));
-          break;
-
-        case 'C':
-          sendCommand(apple_select, sizeof(apple_select) / sizeof(apple_select[0]));
-          break;
-
-        case 'D':
-          sendCommand(apple_up, sizeof(apple_up) / sizeof(apple_up[0]));
-          break;
-
-        case 'E':
-          sendCommand(apple_down, sizeof(apple_down) / sizeof(apple_down[0]));
-          break;
-
-        case 'F':
-          sendCommand(apple_left, sizeof(apple_left) / sizeof(apple_left[0]));
-          break;
-
-        case 'G':
-          sendCommand(apple_right, sizeof(apple_right) / sizeof(apple_right[0]));
-          break;
-
-
-        default:
-          Serial.print("Command unknown");
+    if (userInput == 'd') {
+      debugMode = true;
+      Serial.println("Debug mode initiated");
+    } else if (userInput == 'e') {
+      if (debugMode) {
+        debugMode = false;
+        Serial.println("Debug mode deinitiated");
+      } else {
+        Serial.println("Debug mode is not initiated");
       }
     }
+  }
 
-  */
 
 
+
+  
   if (mySerial.available()) {
+
+    digitalWrite(LED, HIGH);
 
     char recvChar = mySerial.read();
     recvDataString.concat(recvChar);
 
     if (recvChar == '$') {
+      
+      // Remove delimiter
       recvDataString.remove(recvDataString.length()-2);
       
       if (recvDataString == "AppleTV") {
@@ -622,61 +101,96 @@ void loop() {
         
       }
       
+      // Clear for next data
       recvDataString = "";
 
-      Serial.println(commandArrayLen);
+      if (debugMode) {
+        Serial.println();
+        Serial.println("Command length: ");
+        Serial.print(commandArrayLen);
+      }
     }
 
     
 
     if (recvChar == '/') {
-      
+
+      // Remove delimiter
       recvDataString.remove(recvDataString.length()-2);
       
-      iterationsString = recvDataString;
+      numChunksString = recvDataString;
+      
+      // Clear for next data
       recvDataString = "";
 
-      iterationsNum = iterationsString.toInt();
+      numChunks = numChunksString.toInt();
 
-      Serial.println(iterationsString);
+      if (debugMode) {
+        Serial.println();
+        Serial.println("Number of expected chunks: ");
+        Serial.print(numChunks);
+      }
     }
 
     
     if (recvChar == '*') {
-      
-      iterationsCount++;
-      
-      recvDataString.remove(recvDataString.length()-1);
-      
-      Serial.print("Received: ");
-      Serial.println(recvDataString);
 
+      lightOn();
+      
+      numChunksCount++;
+
+      // Remove delimiter
+      recvDataString.remove(recvDataString.length()-1);
+
+      if (debugMode) {
+        Serial.println();
+        Serial.println("Received: ");
+        Serial.print(recvDataString);
+        Serial.print(" from iPhone");
+      }
+     
+      // Send chunk ACK to iPhone 
       mySerial.write("Received Chunk");
+      
+      if (debugMode) {
+        Serial.println();
+        Serial.println("Sending chunk ACK to iPhone...");
+      }
 
       recvTotalDataString.concat(recvDataString);
+
+      // Clear for next data
       recvDataString = "";
 
       // Done receiving
-      if (iterationsCount == iterationsNum) {
-        
-        //Serial.println(recvTotalDataString);
+      if (numChunksCount == numChunks) {
+       
+        if (debugMode) {
+          Serial.println();
+          Serial.println("Constructed string from chunks: ");
+          Serial.print(recvTotalDataString);
+        }
 
+        
         recvTotalDataString += ' ';
 
+        // Send total data ACK to iPhone
         mySerial.write("Received Data");
+        
+        if (debugMode) {
+          Serial.println();
+          Serial.println("Sending total data ACK to iPhone...");
+        }
 
+        
         int command[commandArrayLen];
   
         String num = "";
         int count = 0;
         for (int i = 0; i < recvTotalDataString.length(); i++) {
           
-          if (recvTotalDataString.charAt(i) == ' ') {
-            
-            int commandInt = num.toInt();
-            Serial.println(commandInt);
-            
-            command[count] = commandInt;
+          if (recvTotalDataString.charAt(i) == ' ') {            
+            command[count] = num.toInt();
             count++;
             num = "";
           } else {
@@ -685,29 +199,30 @@ void loop() {
           }
         }
 
+        // Zero out for next time
         recvTotalDataString = "";
-
+        numChunksCount = 0;
           
-//        // send parsed command
-//        for (int i = 0; i < commandArrayLen; i++) {
-//          Serial.println(command[i]);
-//        }
+        // send parsed command
+
+        for (int i = 0; i < commandArrayLen/2; i++) {
+          command[commandArrayLen/2 + i] = command[i];
+        }
+
+        if (debugMode) {
+          for (int i = 0; i < commandArrayLen; i++) {
+            Serial.println();
+            Serial.print("Sending: ");
+            Serial.print(command[i]);
+          }
+        }
+
+
+        sendCommand(command, sizeof(command) / sizeof(command[0]));
+        
       }
-
     }
-    
-
-    //    digitalWrite(LED, HIGH);
-
-  } else {
-    //digitalWrite(LED, LOW);
-
   }
-
-
-
-
-
 }
 
 
@@ -721,11 +236,21 @@ void loop() {
 
 // Sends raw data command to device
 void sendCommand(int rawData[], int len) {
-  Serial.print("Sending");
+  Serial.println();
+  Serial.println("Sending");
   irsend.sendRaw(rawData, len, 38); // 38kHz
-  Serial.print("Sent");
+  Serial.println("Sent");
   delay(100);
+
+  lightOff();
 }
 
 
+
+void lightOn() {
+  digitalWrite(LED, HIGH);
+}
+void lightOff() {
+  digitalWrite(LED, LOW);
+}
 
